bb5e2dcc00f719c9b4f2bf9cb46036fb9b5cb814
Seivan Heidari <seivan@kth.se>
2013-08-06 09:46:26 +0200

    Specs all green.

--- Example/ExampleTests/SHControlBlocksCallbacksIntegration.m
+++ Example/ExampleTests/SHControlBlocksCallbacksIntegration.m
@@ -15,5 +15,44 @@
 
 @implementation SHControlBlocksCallbacksIntegration
 
+#pragma mark - Add block
+-(void)testSH_addControlEvents_withBlock; {
+  __block BOOL didAssert = NO;
+  [self.button SH_addControlEvents:UIControlEventTouchUpInside withBlock:^(UIControl *sender) {
+    STAssertEqualObjects(self.button, sender, nil);
+    didAssert = YES;
+  }];
+  
+  [tester tapViewWithAccessibilityLabel:self.titleButton];
+  STAssertTrue(didAssert, nil);
+  
+  
+}
+
+-(void)testSH_addControlEventTouchUpInsideWithBlock_theBlock; {
+  __block BOOL didAssert = NO;
+  [self.button SH_addControlEventTouchUpInsideWithBlock:^(UIControl *sender) {
+    STAssertEqualObjects(self.button, sender, nil);
+    didAssert = YES;
+  }];
+  
+  [tester tapViewWithAccessibilityLabel:self.titleButton];
+  STAssertTrue(didAssert, nil);
+  
+}
+
+
+-(void)testSample; {
+  __block BOOL didAssert = NO;
+  [self.button SH_addControlEvents:UIControlEventTouchDragExit withBlock:^(UIControl *sender) {
+    STAssertEqualObjects(self.button, sender, nil);
+    STAssertEqualObjects(self.button, sender, nil);
+    didAssert = YES;
+  }];
+  
+  [tester swipeViewWithAccessibilityLabel:self.titleButton inDirection:KIFSwipeDirectionLeft];
+  STAssertTrue(didAssert, nil);
+}
+
 
 @end
--- Example/ExampleTests/SHControlBlocksSelectors.m
+++ Example/ExampleTests/SHControlBlocksSelectors.m
@@ -16,7 +16,7 @@
 @implementation SHControlBlocksSelectors
 
 #pragma mark - Add block
--(void)testSH_addControlEvents_withBlock:(SHControlEventBlock)theBlock; {
+-(void)testSH_addControlEvents_withBlock; {
   __block BOOL didAssert = NO;
   [self.button SH_addControlEvents:UIControlEventTouchUpInside withBlock:^(UIControl *sender) {
     STAssertEqualObjects(self.button, sender, nil);
@@ -42,7 +42,6 @@
 }
 
 
-
 #pragma mark - Remove block
 -(void)testSH_removeControlEventTouchUpInside; {
   __block BOOL didAssert = NO;
@@ -51,12 +50,14 @@
     didAssert = YES;
   }];
   
-  [self.button SH_removeAllControlEventsBlocks];
+  [self.button SH_removeControlEventTouchUpInside];
   [self.button sendActionsForControlEvents:UIControlEventTouchUpInside];
+  STAssertTrue([self.button SH_blocksForControlEvents:UIControlEventTouchUpInside].SH_isEmpty, nil);
   STAssertFalse(didAssert, nil);
   
-
+  
 }
+
 -(void)testSH_removeBlocksForControlEvents; {
   __block BOOL didAssert = NO;
   [self.button SH_addControlEvents:UIControlEventTouchUpInside withBlock:^(UIControl *sender) {
@@ -64,7 +65,8 @@
   }];
   
   [self.button SH_removeBlocksForControlEvents:UIControlEventTouchUpInside];
-  [self.button sendActionsForControlEvents:UIControlEventTouchUpInside];
+  STAssertTrue([self.button SH_blocksForControlEvents:UIControlEventTouchUpInside].SH_isEmpty, nil);
+
   STAssertFalse(didAssert, nil);
 
   
@@ -78,6 +80,8 @@
 
   [self.button SH_removeControlEventsForBlock:self.block];
   [self.button sendActionsForControlEvents:UIControlEventTouchUpInside];
+  STAssertTrue([self.button SH_blocksForControlEvents:UIControlEventTouchUpInside].SH_isEmpty, nil);
+
   STAssertFalse(didAssert, nil);
 
   
@@ -95,8 +99,11 @@
   }];
   
   [self.button SH_removeAllControlEventsBlocks];
-
   [self.button sendActionsForControlEvents:UIControlEventTouchUpInside];
+  STAssertTrue([self.button SH_blocksForControlEvents:UIControlEventTouchUpInside].SH_isEmpty, nil);
+
+  STAssertTrue([self.button SH_blocksForControlEvents:UIControlEventTouchDragInside].SH_isEmpty, nil);
+
   STAssertFalse(didAssertFirstBlock, nil);
   STAssertFalse(didAssertSecondBlock, nil);
 
@@ -105,13 +112,30 @@
 
 #pragma mark - Helpers
 -(void)testSH_blocksForControlEvents; {
+  UIControlEvents event = UIControlEventTouchDragExit;
+  [self.button SH_addControlEventTouchUpInsideWithBlock:self.block];
+  [self.button SH_addControlEvents:event withBlock:self.block];
+  
+  NSSet * blockSet = @[self.block].SH_toSet;
+  STAssertEqualObjects(blockSet,
+                 [self.button SH_blocksForControlEvents:event], nil);
+  
+  STAssertEqualObjects(blockSet,
+                       [self.button SH_blocksForControlEvents:UIControlEventTouchUpInside], nil);
   
 }
 
 -(void)testSH_controlEventsForBlock; {
-  
-}
+  UIControlEvents event = UIControlEventTouchDragExit;
+  [self.button SH_addControlEventTouchUpInsideWithBlock:self.block];
+  [self.button SH_addControlEvents:event withBlock:self.block];
 
+  NSSet * eventSet = [self.button SH_controlEventsForBlock:self.block];
+  STAssertEquals(eventSet.count, 2U, nil);
+  
+  STAssertTrue([eventSet containsObject:@(UIControlEventTouchUpInside)], nil);
+  STAssertTrue([eventSet containsObject:@(event)], nil);
+}
 
 
 #pragma mark - Properties
@@ -119,10 +143,27 @@
 
 #pragma mark - Getters
 -(void)testSH_isTouchUpInsideEnabled; {
-  
+  STAssertFalse(self.button.SH_isTouchUpInsideEnabled, nil);
+  [self.button SH_addControlEventTouchUpInsideWithBlock:self.block];
+  STAssertTrue(self.button.SH_isTouchUpInsideEnabled, nil);
+  [self.button SH_removeControlEventTouchUpInside];
+  STAssertFalse(self.button.SH_isTouchUpInsideEnabled, nil);
 }
 
 -(void)testSH_controlBlocks; {
+  STAssertTrue(self.button.SH_controlBlocks.SH_isEmpty, nil);
+  [self.button SH_addControlEventTouchUpInsideWithBlock:self.block];
+  [self.button SH_addControlEvents:UIControlEventTouchDragExit withBlock:self.block];
+  
+  STAssertEquals(self.button.SH_controlBlocks.count, 2U, nil);
+  STAssertEqualObjects(self.button.SH_controlBlocks[@(UIControlEventTouchDragExit)],
+                       @[self.block].SH_toSet, nil);
+
+  STAssertEqualObjects(self.button.SH_controlBlocks[@(UIControlEventTouchUpInside)],
+                       @[self.block].SH_toSet, nil);
+  
+  [self.button SH_removeAllControlEventsBlocks];
+  STAssertTrue(self.button.SH_controlBlocks.SH_isEmpty, nil);
   
 }
 
